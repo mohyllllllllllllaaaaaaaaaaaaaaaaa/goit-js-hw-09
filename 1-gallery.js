c4481cb37c24685b782de2f6b732a992e6742c39
mohyllllllllllllaaaaaaaaaaaaaaaaa <mogilesha81@gmail.com>
2025-01-30 23:19:40 +0000

    Deploying to gh-pages from @ mohyllllllllllllaaaaaaaaaaaaaaaaa/goit-js-hw-09@f8cb448ab9b4f25982d98d8c22c1d825af40c96d 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/styles-CZ8WjaWo.js";console.log("gallery");
+//# sourceMappingURL=1-gallery.js.map
---
+++ 2-form.js
@@ -0,0 +1,2 @@
+import"./assets/styles-CZ8WjaWo.js";console.log("form");
+//# sourceMappingURL=2-form.js.map
