6be220da3bb53349478ec51e39e14d0299a181ac
natalia mohyla <mogilesha81@gmail.com>
2025-01-31 00:34:14 +0100

    librari

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,7 @@
-console.log('gallery');
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+let lightbox = new SimpleLightbox('.some-element a', { /* options */ });
+console.log(SimpleLightbox);
+
