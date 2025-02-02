ab9b30be866a698737b1c18f0d6747f5702f9e66
natalia mohyla <mogilesha81@gmail.com>
2025-02-02 14:35:35 +0100

    try

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,29 @@
-const form = document.querySelector('.feedback-form');
-const formData = { email: "", message: "" };
+
+const STORAGE_KEY = "feedback-form-state";
+const refs = {
+    form: document.querySelector('.feedback-form')
+};
+const formData = {email: "" , message: ""};
+refs.form.addEventListener('input', (e) => {
+    const email = e.currentTarget.elements.email.value;
+    const message = e.currentTarget.elements.message.value;
+    console.log(formData);
+    saveToLS('formData', data);
+
+});
+function saveToLS(key, value){
+    const jsonData = JSON.stringify(value);
+    localStorage.setItem(key, jsonData);
+};
+function loadFormLS(key){
+    const body = localStorage.getItem(key);
+    try {
+
+    }
+}
+//function saveToLS ()  {
+   // localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+//};
+//const loadFormLS = () =>{
+
+}
