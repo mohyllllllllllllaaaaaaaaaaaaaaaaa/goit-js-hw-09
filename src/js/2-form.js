31f8545410d109088ec6a74fa10f517e50fbe51e
natalia mohyla <mogilesha81@gmail.com>
2025-02-02 15:47:03 +0100

    formfix

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,29 +1,52 @@
 
 const STORAGE_KEY = "feedback-form-state";
 const refs = {
-    form: document.querySelector('.feedback-form')
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input[name="email"]'),
+    message: document.querySelector('.feedback-form textarea[name="message"]'),
 };
-const formData = {email: "" , message: ""};
-refs.form.addEventListener('input', (e) => {
-    const email = e.currentTarget.elements.email.value;
-    const message = e.currentTarget.elements.message.value;
-    console.log(formData);
-    saveToLS('formData', data);
-
-});
-function saveToLS(key, value){
-    const jsonData = JSON.stringify(value);
-    localStorage.setItem(key, jsonData);
+let formData = {
+    email: "" ,
+    message: ""
 };
+function saveToLS(){
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
 function loadFormLS(key){
-    const body = localStorage.getItem(key);
-    try {
-
-    }
-}
-//function saveToLS ()  {
-   // localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    const data = localStorage.getItem(STORAGE_KEY);
+    return data ? JSON.parse(data) : null;}
+   // try {
+//const data = JSON.parse(body);
+//return data;
+  //  } catch{
+   //     return body;
+   // }
 //};
-//const loadFormLS = () =>{
-
+function initPage(){
+    const savedData = loadFormLS();
+    if (savedData){
+        formData = savedData;
+    refs.email.value = savedData.email || "";
+    refs.message.value = formData.message || "";
 }
+}
+initPage();
+
+refs.form.addEventListener('input', (e) => {
+    formData[e.target.name] = e.target.value.trim();
+    saveToLS();
+});
+
+refs.form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    if (!formData.email || !formData.message){
+        alert("Fill please all fields");
+        return;
+    }
+    console.log(formData);
+
+localStorage.removeItem(STORAGE_KEY);
+refs.form.reset();
+formData = { email: "", message: ""};
+});
+
